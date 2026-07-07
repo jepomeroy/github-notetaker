ac761d09490adaeaa3a53dffe1f75c58fff0dfc8
Jon Pomeroy <earl.pomeroy42@gmail.com>
2026-07-07 17:25:02 -0600

    fix: adding favicon

--- app/root.tsx
+++ app/root.tsx
@@ -17,6 +17,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <title>React Github Notetaker</title>
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
                 <Meta />
                 <Links />
                 <script
